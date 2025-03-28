12461abf67fb3a44cd3771ee9809503525ca9727
Wix-Code <ogbonna428alex@gmail.com>
2025-03-28 13:59:28 +0100

    push

---
+++ frontend/app/utils/dummyData.js
@@ -0,0 +1,22 @@
+const NavLinks = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Find Jobs",
+    href: "/find",
+  },
+  {
+    label: "saved Jobs",
+    href: "/save",
+  },
+  {
+    label: "Notifications",
+    href: "/notification",
+  },
+  {
+    label: "Help",
+    href: "/help",
+  },
+]
