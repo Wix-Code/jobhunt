04b777752b92aa0faf93490266acc3fa9705eef2
Wix-Code <ogbonna428alex@gmail.com>
2025-04-08 05:31:16 +0100

    push

--- frontend/app/rooms/page.jsx
+++ frontend/app/rooms/page.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import { data } from '../utils/dummyData';
+"use client"
+import React, { useState } from 'react'
+import { data, icon } from '../utils/dummyData';
 import { TbListDetails } from "react-icons/tb";
-import Rooms from '../subpages/Rooms';
+import Location from '../subpages/Location';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const FindPage = () => {
+  const [menu, setMenu] = useState("Executive")
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
+
   return <div className='bg-[#151719]'>
     <div className='max-w-[1200px] m-auto flex flex-col gap-8'>
       <p className='text-[#b99d75] uppercase text-[12px] font-[600] text-center'>Welcome to Omenma Hotels</p>
       <h1 className='text-[#ffffff] text-[35px] font-[600] text-center'>Explore our refined accommodation options <br /> and find the perfect space for your stay.</h1>
       <p className='text-[#cfcece] text-[17px] text-center'>All rooms have a cozy, ultra clean bed and beddings, bathroom and shower, cable television,<br /> free WIFI and executive seat.</p>
       <div className='flex items-center justify-center gap-3'>
-        <button className='uppercase text-[15px] text-[#FFFFFF] font-[600]'>Executive Rooms</button>
-        <button className='uppercase text-[15px] text-[#FFFFFF] font-[600]'>Deluxe Rooms</button>
-        <button className='uppercase text-[15px] text-[#FFFFFF] font-[600]'>standard Rooms</button>
-        <button className='uppercase text-[15px] text-[#FFFFFF] font-[600]'>royal Rooms</button>
-        <button className='uppercase text-[15px] text-[#FFFFFF] font-[600]'>Event center</button>
+        <button onClick={() => setMenu("Executive")} className='uppercase text-[15px] cursor-pointer text-[#FFFFFF] font-[600]'>Executive Rooms</button>
+        <button onClick={() => setMenu("Deluxe")} className='uppercase text-[15px] text-[#FFFFFF] font-[600] cursor-pointer'>Deluxe Rooms</button>
+        <button onClick={() => setMenu("Standard")} className='uppercase text-[15px] text-[#FFFFFF] cursor-pointer font-[600]'>standard Rooms</button>
+        <button onClick={() => setMenu("Royal")} className='uppercase text-[15px] text-[#FFFFFF] cursor-pointer font-[600]'>royal Rooms</button>
+        <button onClick={() => setMenu("Event")} className='uppercase text-[15px] text-[#FFFFFF] cursor-pointer font-[600]'>Event center</button>
       </div>
       <div className=''>
         {
-          data.map((rooms) => {
+          data.filter((room) => room.filter === menu).map((rooms) => {
             return (
                 <div key={rooms.id} className='flex items-center flex-row gap-20'>
                   <div className='flex-1'>
@@ -43,28 +57,46 @@ const FindPage = () => {
           )
         }
       </div>
-      <div className='grid grid-cols-3 gap-14'>
-        {
-          data.map((rooms) => {
-            return (
-              <div key={rooms.id} className='flex flex-col gap-4'>
-                <img className='w-full object-cover h-[230px]' src={rooms.img} alt="" />
-                <p className='text-[#cfcece] text-[15px] text-center'>{rooms.desc}</p>
-                <button className='text-[#b99d75] text-[16px] cursor-pointer hover:underline'>Discover more</button>
-              </div>
-            )
-          })
-        }
+      <div className='custom-slider-wrapper my-5 relative'>
+        <Slider {...settings}>
+          {
+            data.map((rooms) => {
+              return (
+                <div key={rooms.id} className='flex flex-col gap-4'>
+                  <img className='w-full object-cover h-[230px]' src={rooms.img} alt="" />
+                  <p className='text-[#cfcece] text-[15px] text-center'>{rooms.desc}</p>
+                  <button className='text-[#b99d75] text-[16px] cursor-pointer hover:underline'>Discover more</button>
+                </div>
+              )
+            })
+          }
+        </Slider>
       </div>
     </div>
     <div className='bg-[#000]'>
-      <div className='max-w-[1200px] m-auto py-5'>
+      <div className='max-w-[1200px] m-auto py-10'>
         <p className='text-[#b99d75] uppercase text-[12px] mx-10 font-[600]'>The Essential Omenma Amenities</p>
         <div className='flex flex-row gap-20 mx-10'>
           <p className='text-[#ffffff] flex-1 text-[28px]'>Business or pleasure? We have every amenity you may need to make your stay satisfying.</p>
           <p className='text-[#cfcece] text-[16px] flex-1'>From the moment you enter, you’ll be enveloped in an atmosphere of unparalleled comfort and style, with tastefully appointed furnishings and sumptuous fabrics, lavish bedroom sanctuary, complete with a king-sized bed draped in the finest linens, personalized concierge services, state-of-the-art technology, and unparalleled attention to detail.</p>
         </div>
+        <div className='grid grid-cols-4 gap-10 mt-3'>
+          {
+            icon.map((icon) => {
+              return (
+                <div key={icon.id} className='flex flex-col gap-2'>
+                  <p className='text-[#b99d75] text-[34px]'>{icon.icon}</p>
+                  <p className='text-[#ffffff] text-[22px]'>{icon.name}</p>
+                  <p className='text-[#cfcece] text-[14px]'>{icon.desc}</p>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
+    </div>
+    <div className='py-10'>
+      <Location />
     </div>
   </div>;
 };
--- frontend/app/utils/dummyData.js
+++ frontend/app/utils/dummyData.js
@@ -1,3 +1,5 @@
+import { FaWifi } from "react-icons/fa6"
+
 export const NavLinks = [
   {
     label: "Home",
@@ -31,18 +33,21 @@ export const data = [
   {
     id: 2,
     img: "https://omenmahotels.com/wp-content/uploads/2023/05/Omenma-C-93-1-scaled-e1711455614327-780x705.jpg.webp",
+    filter: "Executive",
     name: "Executive Rooms",
     desc: "Welcome to the epitome of luxury and exclusivity – our most premium hotel room awaits to indulge your senses and exceed your expectations."
   },
   {
     id: 3,
     img: "https://omenmahotels.com/wp-content/uploads/2023/05/Deluxe-2-780x806.jpg.webp",
+    filter: "Royal",
     name: "Royal Rooms",
     desc: "Welcome to the epitome of luxury and exclusivity – our most premium hotel room awaits to indulge your senses and exceed your expectations."
   },
   {
     id: 4,
     img: "https://omenmahotels.com/wp-content/uploads/2024/03/Omenma-C-1-scaled-e1711200148404-600x687.jpg.webp",
+    filter: "Deluxe",
     name: "Deluxe Rooms",
     desc: "Welcome to the epitome of luxury and exclusivity – our most premium hotel room awaits to indulge your senses and exceed your expectations."
   },
@@ -50,11 +55,64 @@ export const data = [
     id: 5,
     img: "https://omenmahotels.com/wp-content/uploads/2024/11/IMG-20241109-WA0003-780x439.jpg.webp",
     name: "Event Center",
+    filter: "Event",
     desc: "Welcome to the epitome of luxury and exclusivity – our most premium hotel room awaits to indulge your senses and exceed your expectations."
   },
   { id:6,
     img: "https://omenmahotels.com/wp-content/uploads/2024/03/Omenma-C-1-scaled-e1711200148404-600x687.jpg.webp",
     name: "Standard Rooms",
+    filter: "Standard",
     desc: "Welcome to the epitome of luxury and exclusivity – our most premium hotel room awaits to indulge your senses and exceed your expectations."
+  },
+]
+
+export const icon = [
+  {
+    id: 1,
+    name: "Airport Pick-up Service",
+    icon: <FaWifi />,
+    desc: "You need to dash to the supermarket? It's downstairs."
+  },
+  {
+    id: 2,
+    name: "Housekeeper Services",
+    icon: <FaWifi />,
+    desc: "You need to dash to the supermarket? It's downstairs."
+  },
+  {
+    id: 3,
+    name: "Free Wifi & Internet",
+    icon: <FaWifi />,
+    desc: "You need to dash to the supermarket? It's downstairs."
+  },
+  {
+    id: 4,
+    name: "24hrs Solar Power Supply",
+    icon: <FaWifi />,
+    desc: "You need to dash to the supermarket? It's downstairs."
+  },
+  {
+    id: 5,
+    name: "Private Parking Space",
+    icon: <FaWifi />,
+    desc: "You need to dash to the supermarket? It's downstairs."
+  },
+  {
+    id: 6,
+    name: "Supermarket",
+    icon: <FaWifi />,
+    desc: "You need to dash to the supermarket? It's downstairs."
+  },
+  {
+    id: 7,
+    name: "Laundry Services",
+    icon: <FaWifi />,
+    desc: "You need to dash to the supermarket? It's downstairs."
+  },
+  {
+    id: 8,
+    name: "24hrs Security",
+    icon: <FaWifi />,
+    desc: "You need to dash to the supermarket? It's downstairs."
   },
 ]
